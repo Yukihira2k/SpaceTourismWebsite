d0492e80058b1414dff71899e8151108113b96a3
Yukihira2k <89770909+Yukihira2k@users.noreply.github.com>
2023-10-12 16:42:52 +0800

    first commit

---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import logo from "../Images/shared/logo.svg";
+import {useState} from "react"
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';  
+
+function Navbar(){
+const navLinks =[
+    {
+        title:"HOME",
+        link:"/Home"
+    },
+    {
+        title:"DESTINATION",
+        link:"/Destination"
+    },
+    {
+        title:"CREW",
+        link:"/Crew"
+    },
+    {
+        title:"TECHNOLOGY",
+        link:"/Technology"
+    },
+]
+
+const [open,setOpen]=useState(false)
+
+function handleMenu(){
+    setOpen((prev)=>!prev);
+}
+    return(
+        <>  
+        <div className="pt-10">
+            <div className="flex justify-around items-center m-auto backdrop-blur-[10px] bg-[#fffffff33]  ">
+                <img src={logo} className="m-auto md:m-10 max-md:absolute max-md:left-10 max-md:top-10"/>
+                <ul className=" gap-auto cursor-pointer hidden md:block md:flex">
+                   {navLinks.map((link,index) => (
+                  <li key={index}href={link.link} className="text-white hover:border-b-2 p-10">{link.title}</li>
+                   ))}
+                </ul>
+
+                    {/* Hamburger link */}
+                
+                    <ul className={` pt-10 gap-auto cursor-pointer ${open? "block text-center"  :"hidden"} md:hidden`}>
+                   {navLinks.map((link,index) => (
+                  <li key={index}href={link.link} className="text-white hover:border-b-2 p-10">{link.title}</li>
+                   ))}
+                </ul>
+                <div className="mr-2 flex block absolute right-10 top-3 md:hidden">
+                    <button onClick={handleMenu} className="text-white m-10">
+                        {open? <CloseIcon/> : <MenuIcon/>}
+                    </button>
+                </div>
+
+           
+
+
+
+            </div>
+
+        </div>
+
+
+      
+
+        </>
+    )
+}
+
+export default Navbar;
